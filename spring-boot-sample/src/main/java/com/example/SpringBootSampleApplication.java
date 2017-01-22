d44aa6bfab15575dee27f2e9ee0c379c6d0893c5
ryu19j <ryu.19j.5@gmail.com>
2017-01-22 21:29:20 +0900

    @ PathVariableで値を取得。

--- spring-boot-sample/src/main/java/com/example/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/example/SpringBootSampleApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,11 @@ public class SpringBootSampleApplication {
 	@GetMapping("/")
 	String home() {
 		return "Hello Spring Boot!";
+	}
+	
+	@GetMapping(path = "{name}")
+	String yourName(@PathVariable String name) {
+		return "Hello " + name + "!";
 	}
 	
 	public static void main(String[] args) {
