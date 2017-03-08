355ef0abd0ef0b86f6ec4dfe9e4ed31948026166
ryu19j <ryu.19j.5@gmail.com>
2017-03-08 23:36:38 +0900

    簡単な帳票を追加。

--- spring-boot-jasper-sample/src/main/java/com/example/controller/TestReportController.java
+++ spring-boot-jasper-sample/src/main/java/com/example/controller/TestReportController.java
@@ -12,9 +12,9 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.data.JRCsvDataSource;
 
 @RestController
 public class TestReportController {
@@ -22,8 +22,10 @@ public class TestReportController {
 	@RequestMapping(value = "/download")
 	public String download(HttpServletResponse response) throws JRException, IOException {
 		File reportFile = new FileSystemResource("src/main/resources/static/reports/TestReport.jasper").getFile();
+		JRCsvDataSource dataSource = new JRCsvDataSource(new FileSystemResource("src/main/resources/static/csv/holiday.csv").getFile());
+	    dataSource.setUseFirstRowAsHeader(true);
 		Map<String, Object> parameter = new HashMap<>();
-        byte[] bytes = JasperRunManager.runReportToPdf(reportFile.getPath(), parameter, new JREmptyDataSource());
+        byte[] bytes = JasperRunManager.runReportToPdf(reportFile.getPath(), parameter, dataSource);
         
         response.setContentType("application/pdf");
         response.setContentLength(bytes.length);
